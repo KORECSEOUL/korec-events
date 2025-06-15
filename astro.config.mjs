c773bc2ae5f80f7f1f60f343d99b9efef9f771e6
Alex Hong <hongalex3@gmail.com>
2025-06-15 17:22:44 +0900

    새로운 GA 4 태그

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,16 +9,22 @@ export default defineConfig({
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/GNUHONG' },
       ],
-      head: [
-        // GTM 코드 바로 위에 삽입!
+       head: [
         {
           tag: 'script',
-          attrs: {},
-          content: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TFMDMLF5');`,
+          attrs: {
+            async: true,
+            src: 'https://www.googletagmanager.com/gtag/js?id=G-1RGL93F9RE'
+          },
+        },
+        {
+          tag: 'script',
+          content: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1RGL93F9RE');
+          `,
         },
       ],
     }),
