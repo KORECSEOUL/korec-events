40d003ae6d7f82f578622fbeb6cc152dc6fe3fb2
Alex Hong <hongalex3@gmail.com>
2025-06-15 16:37:05 +0900

    GitHub UTM 태그  삽입

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,18 @@ export default defineConfig({
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/GNUHONG' },
       ],
+      head: [
+        // GTM 코드 바로 위에 삽입!
+        {
+          tag: 'script',
+          attrs: {},
+          content: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TFMDMLF5');`,
+        },
+      ],
     }),
   ],
 });
