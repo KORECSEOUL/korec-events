7e9c4f2349f960d2b7a103db522f66cd4ba5ced7
Alex Hong <hongalex3@gmail.com>
2025-06-17 13:06:05 +0900

    GTM Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
@@ -9,7 +8,8 @@ export default defineConfig({
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/GNUHONG' },
       ],
-       head: [
+      head: [
+        // 기존 GA4 스크립트
         {
           tag: 'script',
           attrs: {
@@ -24,6 +24,17 @@ export default defineConfig({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-1RGL93F9RE');
+          `,
+        },
+        // 추가된 GTM 스크립트
+        {
+          tag: 'script',
+          content: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5HRKZRKJ');
           `,
         },
       ],
