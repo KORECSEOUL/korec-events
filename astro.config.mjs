8b966bdcab6aec513de4a5de02eb6f92f288bc9f
Alex Hong <hongalex3@gmail.com>
2025-06-20 13:43:14 +0900

    Nav fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  site: 'https://korec-events.pages.dev',
   integrations: [
     starlight({
       title: 'KOREC Event',
