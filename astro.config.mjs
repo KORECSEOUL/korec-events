5fb019a757144b00dc7dccb134136cc3eb339cbb
Alex Hong <hongalex3@gmail.com>
2025-06-20 13:21:40 +0900

    career expo 01 쿼리 테스트

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://korec-events.pages.dev',
   integrations: [
     starlight({
       title: 'KOREC Event',
